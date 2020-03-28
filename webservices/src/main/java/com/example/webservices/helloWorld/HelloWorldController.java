8eaeb87cdc0cf2f10cd57aeb49a896094daffc76
Ashwani Raj <ashwani.raj@hashedin.com>
2020-03-28 14:05:12 +0530

    without swagger

--- webservices/src/main/java/com/example/webservices/helloWorld/HelloWorldController.java
+++ webservices/src/main/java/com/example/webservices/helloWorld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -39,8 +40,14 @@ public class HelloWorldController {
 	}
 
 	@GetMapping(path = "/hello-world-bean-int")
-	public String helloworldBeanInt(@RequestHeader(name="Accept-Language", required=false)Locale locale) {
-		return messageSource.getMessage("good.morning", null, locale);
+	public String helloworldBeanInt() {
+		return messageSource.getMessage("good.morning", null, LocaleContextHolder.getLocale());
 	}
+	
+	
+//	@GetMapping(path = "/hello-world-bean-int")
+//	public String helloworldBeanInt(@RequestHeader(name="Accept-language",required=false)Locale locale) {
+//		return messageSource.getMessage("good.morning", null, locale);
+//	}
 	
 }
